97e26a179e16953c37489a430a0ecc2be97d5069
mgs095 <markus.specogna@gmail.com>
2020-04-25 02:43:51 -0400

    integrate fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
     extend: {},
+    fontFamily: {
+    'title-ext' : ['Aktiv Grotesk Extended'],
+    'title-cond' : ['Aktiv Grotesk'],
+    'body' : ['Caudex'],
+    'mono' : ['IBM Plex Mono']
+    }
   },
   variants: {},
   plugins: [],
