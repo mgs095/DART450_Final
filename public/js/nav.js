c8e44105dc92080a69bc6f3b17f5aaaa789328e1
mgs095 <59260016+mgs095@users.noreply.github.com>
2020-04-29 01:18:39 -0400

    Create nav.js

---
+++ public/js/nav.js
@@ -0,0 +1,8 @@
+// Open/close navigation
+function openNav() {
+    document.getElementById("open-nav").style.width = "100%";
+    }
+
+function closeNav() {
+    document.getElementById("open-nav").style.width = "0%";
+}
